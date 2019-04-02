755e4b077d83dc8955efe2f14a1888985f21f8e0
aliu4 <aliu4@uncc.edu>
2019-04-02 11:59:31 -0400

    added catelog and list

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-var app = angular.module("myApp", ["ui.router", "ui.bootstrap"]);
+var app = angular.module("myApp", ["ui.router", "ui.bootstrap", "firebase"]);
+var phonecatApp = angular.module('recipesList', []);
+
 //Services function as global functions *kinda
 //if you need a global function add a service
 app.factory('myService', function () {
@@ -15,12 +17,29 @@ app.factory('myService', function () {
       };
       firebase.initializeApp(config);
     },
+    userList: function () {
+    },
     checkUser: function () {
       if (firebase.auth().currentUser != null)
         return firebase.auth().currentUser;
     },
   };
 });
+app.factory('recipeService', function($firebaseArray){
+  var recipes;
+  return {
+    getRecipes: getRecipes,
+ 
+    initRecipeList: function () {
+      recipes = $firebaseArray(firebase.database().ref("/recipes").limitToFirst(40));
+      return recipes;
+    },   
+    
+  };
+  function getRecipes(){
+    return recipes;
+  }
+})
 //Remove #! from the url
 app.config(['$locationProvider', function ($locationProvider) {
   $locationProvider.html5Mode(true).hashPrefix('');
@@ -43,9 +62,9 @@ app.config(function ($stateProvider) {
     url: '/profile',
     templateUrl: 'ITCS4155/html/profile.html',
   }
-  var catelogState = {
-    name: 'catelog',
-    url: '/catelog',
+  var catalogState = {
+    name: 'catalog',
+    url: '/catalog',
     templateUrl: 'ITCS4155/html/home.html'
   }
   var recommendState = {
@@ -63,29 +82,83 @@ app.config(function ($stateProvider) {
     url: '/login',
     templateUrl: 'ITCS4155/html/login.html'
   }
+  var recipeState = {
+    name: 'recipe',
+    url: '/recipe/:id',
+    templateUrl: 'ITCS4155/html/recipe.html',
+    controller: function ($stateParams, $scope) {
+      $scope.id = $stateParams.id;
+    },
+  }
   //Allow the website to access the state.
   $stateProvider.state(indexState);
   $stateProvider.state(listState);
   $stateProvider.state(profileState);
-  $stateProvider.state(catelogState);
+  $stateProvider.state(catalogState);
   $stateProvider.state(recommendState);
   $stateProvider.state(favoriteState);
   $stateProvider.state(loginState);
+  $stateProvider.state(recipeState);
 });
 
 //Index Controller
-app.controller('initCtrl', function ($scope, myService) {
+app.controller('initCtrl', function ($scope, myService, $state, $firebaseArray, $window,recipeService) {
   myService.initFirebase();
   $scope.user = myService.checkUser();
+  $scope.recipesList = recipeService.initRecipeList();
+  console.log($scope.recipesList);
+
 });
 //Home Controller
 app.controller('homeCtrl', function ($scope, $state, myService) {
   $scope.user = myService.checkUser();
-})
-//List Controller
-app.controller('listCtrl', function ($scope, $state, myService) {
-  $scope.user = myService.checkUser();
+  $scope.recipesList;
 });
+//Recipe Controller
+app.controller('recipeCtrl', function ($scope, $state, myService, $firebaseArray,recipeService) {
+  $scope.user = myService.checkUser();
+  $scope.recipesList = recipeService.getRecipes();
+  console.log($scope.recipesList);
+
+  $scope.favorited = function () {
+    console.log("Add to favorites");
+  };
+
+  $scope.completed = function () {
+    console.log("Add to completed");
+  }
+});
+
+//List Controller
+app.controller('listCtrl', function ($scope, $state, myService, $firebaseArray) {
+  $scope.user = myService.checkUser();
+  var list;
+  if ($scope.user != null) {
+    list = firebase.database().ref("/list/" + $scope.user.uid);
+  } else {
+    list = firebase.database().ref("/list/0");
+  }
+  $scope.userList = $firebaseArray(list);
+  $scope.add = function () {
+    console.log($scope.userList.$add($scope.ingredient));
+    $scope.ingredient.name = null;
+  }
+  $scope.remove = function (ingredient) {
+    var remove = ingredient;
+    $scope.userList.$remove(remove);
+  }
+  $scope.removeAll = function () {
+    $scope.userList.$remove();
+  }
+});
+//Recomendation Controller
+app.controller('recommendCtrl', function ($scope, $state, myService, $firebaseArray,recipeService) {
+  $scope.user = myService.checkUser();
+  $scope.recipesList = recipeService.getRecipes();
+  var list = firebase.database().ref("/list/0");
+  var userList = $firebaseArray(list);
+});
+
 //Login Controller
 app.controller('loginCtrl', function ($scope, $state, myService) {
   $scope.user = myService.checkUser();
